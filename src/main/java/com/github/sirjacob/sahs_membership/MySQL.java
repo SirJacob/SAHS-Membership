9ec804a7b5c9c8f5d03b2fc312425f00c570c8a3
SirJacob <SirJacob@users.noreply.github.com>
2019-01-27 13:41:00 -0500

    Can now run SELECT stmts via executeQuery method

--- src/main/java/com/github/sirjacob/sahs_membership/MySQL.java
+++ src/main/java/com/github/sirjacob/sahs_membership/MySQL.java
@@ -2,8 +2,10 @@ package com.github.sirjacob.sahs_membership;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,15 +15,15 @@ import java.util.logging.Logger;
  */
 public class MySQL {
 
-    private static final MysqlDataSource dataSource = new MysqlDataSource();
+    private static final MysqlDataSource DATA_SOURCE = new MysqlDataSource();
     private static boolean init = false;
 
     public static void init(MySQLInfo mySQLInfo) {
         if (!init) {
-            dataSource.setUser(mySQLInfo.username);
-            dataSource.setPassword(mySQLInfo.password);
-            dataSource.setServerName(mySQLInfo.ip);
-            dataSource.setDatabaseName(mySQLInfo.database);
+            DATA_SOURCE.setUser(mySQLInfo.username);
+            DATA_SOURCE.setPassword(mySQLInfo.password);
+            DATA_SOURCE.setServerName(mySQLInfo.ip);
+            DATA_SOURCE.setDatabaseName(mySQLInfo.database);
             init = true;
         }
         // TODO: else.. error
@@ -29,17 +31,37 @@ public class MySQL {
 
     /**
      * INSERT, UPDATE, or DELETE
+     *
+     * @param statement
      */
     public static void executeUpdate(String statement) {
-        try {
-            Connection conn = dataSource.getConnection();
-            Statement stmt = conn.createStatement();
+        try (Connection conn = DATA_SOURCE.getConnection(); Statement stmt = conn.createStatement()) {
             stmt.executeUpdate(statement);
 
-            stmt.close();
-            conn.close();
         } catch (SQLException ex) {
             Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).log(Level.SEVERE, null, ex);
         }
+    }
+
+    /**
+     * SELECT
+     *
+     * @param statement
+     * @return
+     */
+    public static HashMap executeQuery(String statement) {
+        HashMap hm = new HashMap();
+        try {
+            Connection conn = DATA_SOURCE.getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(statement);
+
+            hm.put("rs", rs);
+            hm.put("stmt", stmt);
+            hm.put("conn", conn);
+        } catch (SQLException ex) {
+            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).log(Level.SEVERE, null, ex);
+        }
+        return hm;
     }
 }
