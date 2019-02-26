6c4236853cff0b6399e2879f0a04f4191d913170
SirJacob <SirJacob@users.noreply.github.com>
2019-02-26 02:05:03 -0500

    Can recordBreadcrumb() at all severities (levels)

--- src/main/java/com/github/sirjacob/sahs_membership/SentryIO.java
+++ src/main/java/com/github/sirjacob/sahs_membership/SentryIO.java
@@ -1,11 +1,11 @@
 package com.github.sirjacob.sahs_membership;
 
 import io.sentry.Sentry;
+import io.sentry.event.Breadcrumb;
 import io.sentry.event.BreadcrumbBuilder;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -16,20 +16,32 @@ public class SentryIO {
 
     private static boolean init = false;
 
+    //<editor-fold defaultstate="collapsed" desc="Breadcrumb.Level">
+    public static final Breadcrumb.Level DEBUG = Breadcrumb.Level.DEBUG;
+    public static final Breadcrumb.Level INFO = Breadcrumb.Level.INFO;
+    public static final Breadcrumb.Level WARNING = Breadcrumb.Level.WARNING;
+    public static final Breadcrumb.Level ERROR = Breadcrumb.Level.ERROR;
+    public static final Breadcrumb.Level CRITICAL = Breadcrumb.Level.CRITICAL;
+    //</editor-fold>
+
     public static void init() {
         if (!init) {
             try {
                 Sentry.init(Files.readAllLines(new File("SentryDSN.txt").toPath()).get(0));
             } catch (IOException ex) {
-                Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).log(Level.SEVERE, null, ex);
+                Logger.getLogger(Logger.GLOBAL_LOGGER_NAME).log(java.util.logging.Level.SEVERE, null, ex);
             }
             init = true;
         }
     }
 
     public static void recordBreadcrumb(String message) {
+        recordBreadcrumb(message, INFO);
+    }
+
+    public static void recordBreadcrumb(String message, Breadcrumb.Level level) {
         Sentry.getContext().recordBreadcrumb(
-                new BreadcrumbBuilder().setMessage(message).build()
+                new BreadcrumbBuilder().setMessage(message).setLevel(level).build()
         );
     }
 
